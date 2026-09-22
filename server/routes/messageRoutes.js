b648989c12dda4a49ff6a569086b9f5ed755751e
princechouhan8171-stack <princechouhan8171@gmail.com>
2026-09-22 14:03:50 +0530

    Add server/routes/messageRoutes.js

---
+++ server/routes/messageRoutes.js
@@ -0,0 +1,1 @@
+const express=require('express');const router=express.Router();const {getMessages,sendMessage}=require('../controllers/messageController');const {protect}=require('../middleware/authMiddleware');router.use(protect);router.get('/:conversationId',getMessages);router.post('/',sendMessage);module.exports=router;
